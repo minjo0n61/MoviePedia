6f3aa0e5737dd10f5016e63d7a92d3e729d438fd
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-08 20:05:07 +0900

    feat: Add handleLoadClick function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import ReviewList from './ReviewList';
-import mockItems from '../mock.json';
+import getReviews from '../api';
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -14,6 +14,11 @@ function App() {
     // 해당 id값이랑 다른 것들만 새로 items 배열로 만들어서 넣어주고 선택한 한개의 id는 제외되는 것
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
+  };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
   };
 
   return (
@@ -27,6 +32,9 @@ function App() {
         </button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button type="button" onClick={handleLoadClick}>
+        불러오기
+      </button>
     </div>
   );
 }
