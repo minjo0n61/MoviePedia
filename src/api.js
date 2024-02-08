522bc05b3c3219455545c7ecbe3b2e7208d7c711
MinJoon_Choi <minjoon1998@gmail.com>
2024-02-08 20:42:47 +0900

    feat: Add order parameter in getReviews function

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
-async function getReviews() {
-  const response = await fetch('https://learn.codeit.kr/2728/film-reviews');
+async function getReviews(order = 'createdAt') {
+  const query = `order=${order}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/2728/film-reviews?${query}`,
+  );
   const body = await response.json();
   return body;
 }
